1391374682b63ace02f836c1226fb85f782d4ec6
Maria Reedstrom <maria.reedstrom@gmail.com>
2021-11-24 09:56:02 -0600

    commit tree af1acb669be6cafd8d2297e37a1aedf2cf65c7c8

--- index.js
+++ index.js
@@ -1,18 +1,49 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const string = word.toLowerCase();
+
+  const wordReverse = string.split("").reverse().join("");
+  return string === wordReverse;
 }
 
 /* 
-  Add your pseudocode here
+  Add your pseudocode here:
+receive string argument 
+
+make string all lowercase 
+
+split string into array, reverse array, join back to string
+
+if input is strictly equal to reversed word
+  return true 
+else
+  return false
+
+---------- or ------------
+
+reverse string
+ if the input is the same as the return input
+  return true
+ else 
+  return false
+
+
 */
 
 /*
   Add written explanation of your solution here
+  First made sure word was all lower case to be able to compare. 
+  The split word into array, in order to reverse array, then merged array back to a string with join. 
+  Finally compare if reversed word is equal to the input. 
+
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+
+  // mom === mom => true
+  // car === rac => false
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -20,6 +51,12 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("Expecting true:");
+  console.log("=>", isPalindrome("CivIC"));
+
+  console.log("Expecting false:");
+  console.log("=>", isPalindrome("civil"));
 }
 
 module.exports = isPalindrome;
